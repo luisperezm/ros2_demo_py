da7fba41e612472cb2e39f15359ecc21ca490033
Luis Alejandro Pérez Mínguez <lap.minguez@gmail.com>
2019-11-02 03:25:11 -0700

    la

--- install/setup.sh
+++ install/setup.sh
@@ -34,6 +34,10 @@ _colcon_prefix_chain_sh_source_script() {
 COLCON_CURRENT_PREFIX="/opt/ros/crystal"
 _colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
 
+# setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
+COLCON_CURRENT_PREFIX="/home/luis/Documents/script_example/test/ros2_demo_py/ros2_demo_py/ros2_demo_py/install"
+_colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
+
 
 # source this prefix
 # setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
