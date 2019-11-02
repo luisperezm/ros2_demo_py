d7e1b6bdd3526c4b1e0e0534439e9a7e9ac76adc
Luis Alejandro Pérez Mínguez <lap.minguez@gmail.com>
2019-11-02 03:21:40 -0700

    hi

--- ros2_demo_py/install/setup.ps1
+++ ros2_demo_py/install/setup.ps1
@@ -23,6 +23,7 @@ function _colcon_prefix_chain_powershell_source_script {
 
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/crystal\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/luis/Documents/script_example/test/ros2_demo_py/ros2_demo_py/ros2_demo_py/install\local_setup.ps1"
 
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
